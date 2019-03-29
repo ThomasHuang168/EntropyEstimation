b6c369a03de626ee17d183defeca74ef44a6f71e
Huang Hing Pang <Thomas@Huangs-MacBook-Air.local>
2019-03-29 17:38:54 +0800

    update Reg formular

--- MINE_Upper_bound.py
+++ MINE_Upper_bound.py
@@ -1,21 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 
-
-# In[2]:
-
-
+Input_size_Mine = 4
 class Mine(nn.Module):
-    def __init__(self, input_size=2, hidden_size=100):
+    def __init__(self, input_size=Input_size_Mine, hidden_size=100):
         super().__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
@@ -32,10 +23,6 @@ class Mine(nn.Module):
         output = F.elu(self.fc2(output))
         output = self.fc3(output)
         return output
-
-
-# In[3]:
-
 
 def mutual_information(joint, marginal, mine_net):
     t = mine_net(joint)
@@ -61,10 +48,6 @@ def learn_mine(batch, mine_net, mine_net_optim,  ma_et, ma_rate=0.01):
     mine_net_optim.step()
     return mi_lb, ma_et
 
-
-# In[4]:
-
-
 def sample_batch(data, batch_size=100, sample_mode='joint'):
     if sample_mode == 'joint':
         index = np.random.choice(range(data.shape[0]), size=batch_size, replace=False)
@@ -75,11 +58,10 @@ def sample_batch(data, batch_size=100, sample_mode='joint'):
         batch = np.concatenate([data[joint_index][:,0].reshape(-1,1),
                                          data[marginal_index][:,1].reshape(-1,1)],
                                        axis=1)
+        for i in range(2, Input_size_Mine):
+            marginal_index = np.random.choice(range(data.shape[0]), size=batch_size, replace=False)
+            batch = np.append(batch, data[marginal_index][:,i].reshape(-1,1), axis=1)
     return batch
-
-
-# In[5]:
-
 
 def train(data, mine_net,mine_net_optim, batch_size=100, iter_num=int(5e+3), log_freq=int(1e+3), verbose=True):
     # data is x or y
@@ -93,16 +75,8 @@ def train(data, mine_net,mine_net_optim, batch_size=100, iter_num=int(5e+3), log
             print(result[-1])
     return result
 
-
-# In[6]:
-
-
 def ma(a, window_size=100):
     return [np.mean(a[i:i+window_size]) for i in range(0,len(a)-window_size)]
-
-
-# In[7]:
-
 
 import numpy as np
 import pandas as pd
@@ -111,23 +85,11 @@ import math
 from scipy.stats import randint
 import DiscreteCondEnt as DC
 
-
-# In[8]:
-
-
 from sklearn.linear_model import LinearRegression
 linReg = LinearRegression()
 
-
-# In[9]:
-
-
 def varEntropy(y):
     return np.log(np.var(y)*3.14159*2)/2
-
-
-# In[11]:
-
 
 from sklearn.metrics import mean_squared_error
 def MSEscorer(clf, X, y):
@@ -137,41 +99,38 @@ def MSEscorer(clf, X, y):
 
 # In[ ]:
 
+GT2_const = Input_size_Mine * 0.5 * np.log(2*np.pi*np.e)
 
+CVFold = 3
 MINE2 = []
 LinReg2 = []
 GT2 = []
-COV2 = []
-CVFold = 3
-for i in range(1, 16):
-    cov = 1 - 0.1**i
-    COV2.append(cov)
-    x = np.transpose(np.random.multivariate_normal( mean=[0,0],
-                                  cov=[[1,cov],[cov,1]],
-                                 size = 1000))
+#COV2 = []
+#for i in range(1, 16):
+#    cov = 1 - 0.1**
+#    COV2.append(cov)i
+COV2 = np.linspace(0, 0.9, 5)
+for cov in COV2:
+    Gaussian_cov = [[1,0,0,cov],[0,1,0,cov],[0,0,1,cov],[cov,cov,cov,1]]
+    x = np.transpose(np.random.multivariate_normal( mean=[0,0,0,0],
+                                  cov=Gaussian_cov,
+                                 size = 300))
     DE = DC.computeEnt(x, linReg, MSEscorer, varEntropy, CVFold)
     MI = DE[1,0] + DE[0,0] - DE[0,1] - DE[1,1]
     MI = MI/2
     LinReg2.append(MI)
-    #plt.scatter(cov, MI, c='g',label='KNN-regressor')
-    groundTruth = -0.5*np.log(1-cov*cov)
+    
+    #groundTruth = -0.5*np.log(1-cov*cov)
+    groundTruth = -0.5*np.log(np.linalg.det(np.array(Gaussian_cov)))
     GT2.append(groundTruth)
-    #plt.scatter(cov, groundTruth, c='r',label='ground truth')
     
     #MINE
     mine_net = Mine()
     mine_net_optim = optim.Adam(mine_net.parameters(), lr=1e-3)
-    result = train(np.transpose(x),mine_net,mine_net_optim, verbose=False)
+    result = train(np.transpose(x),mine_net,mine_net_optim)
     result_ma = ma(result)
     MINE2.append(result_ma[-1])
     #MINE
-
-#plt.legend()
-#plt.show()
-
-
-# In[14]:
-
 
 fig, ax = plt.subplots()
 
@@ -181,10 +140,6 @@ ax.scatter(COV2, GT2, c='g', label='Ground Truth')
 
 ax.legend()
 plt.show()
-
-
-# In[16]:
-
 
 fig2, ax2 = plt.subplots()
 COV22 = np.log(np.ones(len(COV2)) - COV2)
